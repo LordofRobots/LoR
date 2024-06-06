76a76ba68df622f1b1670167c9aa72a28bb8dc70
Lord of Robots <131721361+LordofRobots@users.noreply.github.com>
2024-06-06 10:48:19 -0400

    library-o-fy process
    moved io and LoR_Core specific items into LoR library

--- examples/LoR_Core_BluePad32/LoR_Core_BluePad32.ino
+++ examples/LoR_Core_BluePad32/LoR_Core_BluePad32.ino
@@ -1,5 +1,5 @@
 /**
- * fLoR_Core_BluePad32.ino
+ * LoR_Core_BluePad32.ino
  * 
  * Description:
  * This project file integrates the LoR (Lord of Robots) library to control a robotic platform
@@ -56,108 +56,8 @@ void INIT_Serial(){
 }
 
 //////////////////////////////////////////////////////////////////////////
-/////              Motor config and functions                        /////
+/////                      Motion functions                          /////
 //////////////////////////////////////////////////////////////////////////
-
-//GPIO SETUP ------------------------------------------------------------
-void INIT_GPIO() {
-  // Set up the pins
-  pinMode(LED_DataPin, OUTPUT);
-  pinMode(SwitchPin, INPUT_PULLUP);
-  pinMode(MotorEnablePin, OUTPUT);
-  digitalWrite(MotorEnablePin, 0);
-
-
-  for (int i = 0; i < 6; i++) {
-    pinMode(motorPins_A[i], OUTPUT);
-    pinMode(motorPins_B[i], OUTPUT);
-    digitalWrite(motorPins_A[i], 0);
-    digitalWrite(motorPins_B[i], 0);
-  }
-
-  delay(1000);  // to encurage startup stability
-
-  // output preset bias
-  digitalWrite(LED_DataPin, 0);
-  digitalWrite(MotorEnablePin, 1);
-
-  //Motor test tones
-  Start_Tone();
-}
-
-// PWM CONFIG ------------------------------------------------------------
-// configure LED PWM functionalitites
-void INIT_PWM() {
-  for (int i = 0; i < 6; i++) {
-    ledcSetup(MOTOR_PWM_Channel_A[i], PWM_FREQUENCY, PWM_RESOLUTION);
-    ledcSetup(MOTOR_PWM_Channel_B[i], PWM_FREQUENCY, PWM_RESOLUTION);
-    ledcAttachPin(motorPins_A[i], MOTOR_PWM_Channel_A[i]);
-    ledcAttachPin(motorPins_B[i], MOTOR_PWM_Channel_B[i]);
-    // ledcWrite(motorPins_A[i], 0);
-    // ledcWrite(motorPins_B[i], 0);
-  }
-}
-
-// STARTUP TONES ------------------------------------------------------
-// Auditable Tones created in the motors. Cycle through each motor.
-void Start_Tone() {
-  for (int i = 0; i < 6; i++) {
-    long ToneTime = millis() + 200;
-    bool state = 0;
-    while (millis() < ToneTime) {
-      digitalWrite(motorPins_A[i], state);
-      digitalWrite(motorPins_B[i], !state);
-      state = !state;
-      long WaitTime = micros() + (100 * (i + 1));
-      while (micros() < WaitTime) {}
-    }
-    digitalWrite(motorPins_A[i], 0);
-    digitalWrite(motorPins_B[i], 0);
-    delay(50);
-  }
-  for (int i = 0; i < 6; i++) {  // most likely not needed
-    digitalWrite(motorPins_A[i], 0);
-    digitalWrite(motorPins_B[i], 0);
-  }
-}
-
-// SLEW RATE ------------------------------------------------------------
-//Function to handle slew rate for motor speed ramping
-// Slew rate for ramping motor speed
-int SLEW_RATE_MS = 200;
-int SlewRateFunction(int Input_Target, int Input_Current) {
-  int speedDiff = Input_Target - Input_Current;
-  if (speedDiff > 0) Input_Current += min(speedDiff, SLEW_RATE_MS);
-  else if (speedDiff < 0) Input_Current -= min(-speedDiff, SLEW_RATE_MS);
-  Input_Current = constrain(Input_Current, -512, 512);
-  return Input_Current;  //// BYPASSED
-}
-
-//MOTOR OUTPUT CONTROL --------------------------------------------------
-// Function to control motor output based on input values from -512 to 512
-// PWM config: 8bit @ 20,000hz
-// Motor speed limits and starting speed
-const int MAX_SPEED = 255;
-const int MIN_STARTING_SPEED = 100;  // to overcome internal friction inside motor assembly
-const int STOP = 0;
-void Set_Motor_Output(int Output, int Motor_ChA, int Motor_ChB) {
-  Output = constrain(Output, -512, 512);
-  int Mapped_Value = map(abs(Output), 0, 512, MIN_STARTING_SPEED, MAX_SPEED);
-  int A, B = 0;
-  if (Output < -1) {  // Rotate Clockwise
-    A = 0;
-    B = Mapped_Value;
-  } else if (Output > 1) {  // Rotate Counter-Clockwise
-    A = Mapped_Value;
-    B = 0;
-  } else {  // Rotation Stop
-    A = STOP;
-    B = STOP;
-  }
-  ledcWrite(Motor_ChA, A);  //send to motor control pins
-  ledcWrite(Motor_ChB, B);
-}
-
 //MOTION CONTROL ALGORITHM ---------------------------------------------
 // Process joystick input and calculate motor speeds
 const float TURN_RATE = 1;
@@ -184,20 +84,19 @@ void Motion_Control(int ForwardBackward_Axis, int TurnLeftRight_Axis) {
   RIGHT_TargetValue = constrain(RIGHT_TargetValue, -512, 512);
 
   //set motor speed through slew rate function
-  SLEW_RATE_MS = 300;
-  Motor_LEFT_SetValue = SlewRateFunction(LEFT_TargetValue, Motor_LEFT_SetValue);
-  Motor_RIGHT_SetValue = SlewRateFunction(RIGHT_TargetValue, Motor_RIGHT_SetValue);
+  Motor_LEFT_SetValue = LoR.SlewRateFunction(LEFT_TargetValue, Motor_LEFT_SetValue, 300);
+  Motor_RIGHT_SetValue = LoR.SlewRateFunction(RIGHT_TargetValue, Motor_RIGHT_SetValue, 300);
 }
 
 // MINIBOT 3 MOTOR DRIVE ----------------------------------------------
 //send values to all motors
-void DriveControl(int LEFT, int RIGHT) {
-  Set_Motor_Output(LEFT, Motor_M1_A, Motor_M1_B);
-  Set_Motor_Output(LEFT, Motor_M2_A, Motor_M2_B);
-  Set_Motor_Output(LEFT, Motor_M3_A, Motor_M3_B);
-  Set_Motor_Output(-RIGHT, Motor_M4_A, Motor_M4_B);
-  Set_Motor_Output(-RIGHT, Motor_M5_A, Motor_M5_B);
-  Set_Motor_Output(-RIGHT, Motor_M6_A, Motor_M6_B);
+void Drive_Control(int LEFT, int RIGHT) {
+  LoR.Set_Motor_Output(LEFT, Motor_M1_A, Motor_M1_B);
+  LoR.Set_Motor_Output(LEFT, Motor_M2_A, Motor_M2_B);
+  LoR.Set_Motor_Output(LEFT, Motor_M3_A, Motor_M3_B);
+  LoR.Set_Motor_Output(-RIGHT, Motor_M4_A, Motor_M4_B);
+  LoR.Set_Motor_Output(-RIGHT, Motor_M5_A, Motor_M5_B);
+  LoR.Set_Motor_Output(-RIGHT, Motor_M6_A, Motor_M6_B);
 }
 
 
@@ -219,15 +118,15 @@ const uint32_t PURPLE = strip.Color(255, 0, 255, 0);
 const uint32_t CYAN = strip.Color(0, 255, 255, 0);
 const uint32_t YELLOW = strip.Color(255, 255, 0, 0);
 
-// Neopixels Configuration
+// LED Configuration
 void INIT_rgbLED() {
   strip.begin();            // INITIALIZE NeoPixel strip object
   strip.show();             // Turn OFF all pixels ASAP
   strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
-  NeoPixel_SetColour(BLUE);
+  LED_SetColour(BLUE);
 }
 
-void NeoPixel_SetColour(uint32_t color) {
+void LED_SetColour(uint32_t color) {
   for (int i = 0; i < strip.numPixels(); i++) {  // For each pixel in strip...
     strip.setPixelColor(i, color);               //  Set pixel's color (in RAM)
     strip.show();                                // Update strip with new contents
@@ -278,7 +177,7 @@ void onConnectedController(ControllerPtr ctl) {
 void onDisconnectedController(ControllerPtr ctl) {
   if (myController == ctl) {
     Serial.println("Controller disconnected");
-    NeoPixel_SetColour(RED);
+    LED_SetColour(RED);
     myController = nullptr;  // Reset the controller pointer when disconnected
   }
 }
@@ -324,14 +223,14 @@ void CheckController_Battery() {
 }
 
 // GAME PAD COMPATIBILITY -----------------------------------------------
-void processGamePad() {
+void Process_GamePad() {
   if (myController->isGamepad()) {
     dumpGamepad(myController);
-    NeoPixel_SetColour(GREEN);
+    LED_SetColour(GREEN);
     //CheckController_Battery();
   } else {
     Serial.println("Unsupported controller");
-    NeoPixel_SetColour(YELLOW);
+    LED_SetColour(YELLOW);
   }
 }
 
@@ -345,27 +244,25 @@ void setup() {
   INIT_rgbLED();
   LoR.begin();  // Initialize the LoR library
   INIT_BluePad32();
-  INIT_GPIO();
-  INIT_PWM();
-  NeoPixel_SetColour(RED);
+
+  LED_SetColour(RED);
   Serial.println("MiniBot: CORE System Ready! ");
 }
 
 
 ///////////////////////////////////////////////////////////////////////
-//                          Main SETUP                               //
+//                          Main Loop                               //
 ///////////////////////////////////////////////////////////////////////
 
 void loop() {
 
   BP32.update();
   if (myController && myController->isConnected()) {
-    processGamePad();
+    Process_GamePad();
     Motion_Control(myController->axisY(), myController->axisRX());  // Joystick control
-    DriveControl(Motor_LEFT_SetValue, Motor_RIGHT_SetValue);
+    Drive_Control(Motor_LEFT_SetValue, Motor_RIGHT_SetValue);
   }
   else {  //Stop/Standby
-
-    DriveControl(STOP, STOP);
+    Drive_Control(STOP, STOP);
   }
 }
